69356e7d10736c5192cbba9b1cf92f0ca97c6094
Andre Antonio <andreantoniosilva96@gmail.com>
2026-02-07 09:04:46 -0300

    adiciona configuração e logger para a aplicação, incluindo estrutura de abertura de oportunidades

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/andreantoniodev/gopportunities.git/router"
+import (
+	"github.com/andreantoniodev/gopportunities.git/config"
+	"github.com/andreantoniodev/gopportunities.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Failed to initialize configuration: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
